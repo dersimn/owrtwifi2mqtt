357d6d7df3ec2ac2032be2f8582498d7d33e59cf
Simon Christmann <simon@christmann.email>
2020-05-14 16:53:09 +0200

    Simplify example

--- example/mqtt-scripts/presence.js
+++ example/mqtt-scripts/presence.js
@@ -1,18 +1,8 @@
-subscribe('$presence/count', {
-        change: true,
-        condition: (topic, val, obj, oldObj) => {
-            if (oldObj.val == 0 && val != 0) { // Exact change from 0 -> (!0)
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }, () => {
-        log.info('First one arrived at home');
+subscribe('$presence/count', {change: true, condition: 'objPrev.val == 0 && val != 0'}, () => { // Exact change from 0 -> (!0)
+    log.info('First one arrived at home');
 
-        // ... actual logic here
-    }
-);
+    // ... actual logic here
+});
 
 subscribe('$presence/count', {change: true, condition: 'val == 0'}, () => {
     log.info('Everyone left the appartment');
